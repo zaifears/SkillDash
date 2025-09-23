65053a0583d9fdcbd654385a51166644201b1f56
SHANJIDA TABASSUM SHOILY <shanjidatabassum-2021811159@thm.du.ac.bd>
2025-09-23 04:52:19 +0000

    Revert "Homepage issue"
    This reverts commit 33d7fa9b66884c5051058ddfb809b48c331936bb.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem={true}
+          enableSystem
           disableTransitionOnChange={false}
         >
           <ModernNavbar />
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,31 +1,33 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-interface Props {
-  children: React.ReactNode
-  attribute?: "class" | "data-theme" | "data-mode"
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
 }
 
-export function ThemeProvider({
-  children,
+export function ThemeProvider({ 
+  children, 
   attribute = "class",
   defaultTheme = "system",
   enableSystem = true,
   disableTransitionOnChange = false,
-}: Props) {
+  ...props 
+}: ThemeProviderProps) {
   return (
-    <NextThemesProvider
+    <NextThemesProvider 
       attribute={attribute}
       defaultTheme={defaultTheme}
       enableSystem={enableSystem}
       disableTransitionOnChange={disableTransitionOnChange}
+      {...props}
     >
       {children}
     </NextThemesProvider>
-  )
+  );
 }
