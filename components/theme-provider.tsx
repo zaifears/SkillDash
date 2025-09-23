33d7fa9b66884c5051058ddfb809b48c331936bb
SHANJIDA TABASSUM SHOILY <shanjidatabassum-2021811159@thm.du.ac.bd>
2025-09-23 04:49:00 +0000

    Homepage issue

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,33 +1,31 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-interface ThemeProviderProps {
-  children: React.ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
+interface Props {
+  children: React.ReactNode
+  attribute?: "class" | "data-theme" | "data-mode"
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
 }
 
-export function ThemeProvider({ 
-  children, 
+export function ThemeProvider({
+  children,
   attribute = "class",
   defaultTheme = "system",
   enableSystem = true,
   disableTransitionOnChange = false,
-  ...props 
-}: ThemeProviderProps) {
+}: Props) {
   return (
-    <NextThemesProvider 
+    <NextThemesProvider
       attribute={attribute}
       defaultTheme={defaultTheme}
       enableSystem={enableSystem}
       disableTransitionOnChange={disableTransitionOnChange}
-      {...props}
     >
       {children}
     </NextThemesProvider>
-  );
+  )
 }
