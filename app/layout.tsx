f00a1d32a519f9c2e70b19ba881208f9943f7943
SHANJIDA TABASSUM SHOILY <shanjidatabassum-2021811159@thm.du.ac.bd>
2025-10-01 06:25:13 +0000

    1.3.8 Deffered Layout Loading in Mobile Phone

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,15 +121,17 @@ export default function RootLayout({
         {/* Canonical URL */}
         <link rel="canonical" href="https://skilldash.live" />
         
-        {/* 🔧 GTM HEAD SCRIPT - UNCHANGED (Don't touch this!) */}
+        {/* 🚀 DEFERRED GTM - Loads AFTER page loads (MOBILE FIX) */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
+              window.addEventListener('load', function() {
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              });
             `
           }}
         />
